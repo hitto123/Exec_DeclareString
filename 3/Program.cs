34b09630a010d3b1d033dd88c3a4a5ee66503ae0
hitto <hitto@DESKTOP-A07E2BV>
2022-10-16 23:41:28 +0800

    加入專案檔案。

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "我是第一行\r\n我是第二行";
+            Console.WriteLine(word);
+        }
+    }
+}
